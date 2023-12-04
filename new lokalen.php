5b373c514eed33daae2a1d1ed7985b7da8f7aede
Ice Laforteza <118431316+IceLaforteza@users.noreply.github.com>
2023-12-04 14:53:18 +0100

    Create new lokalen.php

---
+++ new lokalen.php
@@ -0,0 +1,69 @@
+Dit is code met een donkere kleur op die dingen.
+<?php
+
+$lokalen = [
+    "lokaal1" => [
+        "lokaalnaam" => "1.073",
+        "capaciteit" => 28
+    ],
+    "lokaal2" => [
+        "lokaalnaam" => "1.28",
+        "capaciteit" => 30
+    ],
+    "lokaal3" => [
+        "lokaalnaam" => "1.078",
+        "capaciteit" => 32
+    ],
+    "lokaal4" => [
+        "lokaalnaam" => "1.064",
+        "capaciteit" => 28
+    ]
+];
+
+echo "<ul>";
+
+foreach ($lokalen as $key => $lokaal) {
+    echo "<li>";
+
+    echo sprintf("<strong>%s</strong> - Capaciteit: %d mensen", $lokaal["lokaalnaam"], $lokaal["capaciteit"]);
+
+    echo "</li>";
+}
+
+echo "</ul>";
+?>
+/////////////////////////////
+
+<?php
+
+$lokalen = [
+    "lokaal1" => [
+        "lokaalnaam" => "1.073",
+        "capaciteit" => 28
+    ],
+    "lokaal2" => [
+        "lokaalnaam" => "1.28",
+        "capaciteit" => 30
+    ],
+    "lokaal3" => [
+        "lokaalnaam" => "1.078",
+        "capaciteit" => 32
+    ],
+    "lokaal4" => [
+        "lokaalnaam" => "1.064",
+        "capaciteit" => 28
+    ]
+];
+
+echo "<ul>";
+
+foreach ($lokalen as $key => $lokaal) {
+    echo "<li>";
+    
+    echo sprintf("%s - Capaciteit: %d mensen", $lokaal["lokaalnaam"], $lokaal["capaciteit"]);
+
+    echo "</li>";
+}
+
+echo "</ul>";
+?>
